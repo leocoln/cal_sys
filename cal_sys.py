063150ead91d1b225a890ba0f93bc2651d76b724
zeng xin <70895921+leocoln@users.noreply.github.com>
2020-10-09 20:57:49 +0800

    Create cal_sys.py

---
+++ cal_sys.py
@@ -0,0 +1,58 @@
+import random
+from fractions import Fraction
+
+op_standrad = ['+','-','×','÷']
+
+def mdfk():
+	op_amount = random.randint(1,3)
+	op = []
+	num = 0
+	swap = 0
+	danger = 0
+	que = get_num()
+	question = [que]
+	for i in range(op_amount):
+		op.append(random.choice(op_standrad))
+	
+	for op_sign in op:
+		if op_sign == '+':
+			que += get_num()
+			danger += 1
+		
+		if op_sign == '-':
+			num = get_num()
+			if que > num : 
+				que -= num
+			else: 
+				swap = 1
+				que = num - que
+			danger += 1
+
+		if op_sign == '×':
+			que *= get_num
+		
+		if op_sign == '÷':
+			#if num == 0 : num = get_num()
+			#if num > que : que =
+			print("mismatch")
+			break
+		
+		if op_sign == '×' or op_sign == '÷':
+			if danger >= 1:
+				question = ['('] + question + [')']
+
+		if swap == 1 :
+			question = [num] + [op_sign] + question
+			swap = 0
+		else:
+			question = question + [op_sign] + [num]
+	question = ' '.join('%s' %id for id in question)
+	print (question)
+
+def get_num():
+	i = 0
+	i = random.randint(0,9)
+	return i
+
+if __name__ == "__main__":
+	mdfk()
